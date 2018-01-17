fbbc0bd62cd539c697ecdc1c627389366f97dd50
marius <lfmars@gmail.com>
2018-01-17 08:56:10 +0200

    cant get date from sql

--- StackDocsFlow/Program.cs
+++ StackDocsFlow/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+using StackDocsFlow.Services;
+using StackDocsFlow.Services.Impl;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +18,17 @@ namespace StackDocsFlow
         [STAThread]
         static void Main()
         {
+      IDataConnection connection = new DataConnection();
+     // DataTable dt = connection.selectQuery("select strftime('%m/%d/%Y',substr(creationDate,0,20)) from docTags");
+      DataTable dt = connection.selectQuery("select id, title from docTags");
+      //    DataTable dt = connection.selectQuery("select title from docTags");
+
+      foreach (DataRow row in dt.Rows)
+      {
+        //string x = row["creationDate"].ToString();
+        //     var f = 4;
+        Console.Write(row);
+      }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
---
+++ StackDocsFlow/Services/Impl/DataConnection.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDocsFlow.Services.Impl
+{
+  class DataConnection : IDataConnection
+  {
+    private SQLiteConnection sqlite;
+
+    public DataConnection()
+    {
+      string x = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+    //  string rootDir= System.IO.Directory.GetCurrentDirectory();
+      string pathName = x + "\\ExternalSources\\maindb.db";
+      //  C: \Users\marius\source\repos\SeeProject\StackDocsFlow\ExternalSources\maindb.db
+      sqlite = new SQLiteConnection("Data Source ="+pathName);
+    }
+
+    public DataTable selectQuery(string query)
+    {
+      SQLiteDataAdapter adapter;
+      DataTable dataTable = new DataTable();
+      try
+      {
+        SQLiteCommand cmd;
+        sqlite.Open();
+        cmd = sqlite.CreateCommand();
+        cmd.CommandText = query;
+        adapter = new SQLiteDataAdapter(cmd);
+        adapter.Fill(dataTable);
+      }
+      catch(SQLiteException ex)
+      {
+        Console.WriteLine("Error in DataConnection: selectQuerry" + ex);
+      }
+      sqlite.Close();
+      Console.WriteLine("uzsidarom");
+      return dataTable;
+
+    
+    }
+  }
+}
